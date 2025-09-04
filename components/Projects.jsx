c8a346735dc8bcce26f74cb9551de1c1ca95f7fe
Ritikyadav09 <ritikydv0909@gmail.com>
2025-09-04 10:49:59 +0530

    ok

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -6,16 +6,21 @@ const Projects = () => {
         <h3 className="text-3xl font-bold mb-12 text-center">Projects</h3>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
           <div className="bg-gray-800 p-6 rounded-xl shadow hover:shadow-lg">
-            <h4 className="text-xl font-semibold">Project 1</h4>
-            <p className="mt-2 text-gray-400">A cool web app built with Next.js and TailwindCSS.</p>
+            <h4 className="text-xl font-semibold">E-Commerce Website</h4>
+            <p className="mt-2 text-gray-400">A Fully Working E-Commerce Frontend built with Next.js and TailwindCSS.</p>
+            <a className=" underline mr-10" href="https://e-commerce-khaki-nine-23.vercel.app/">View Project</a>
+            <a className=" underline" href="https://github.com/rritikyadav/E-Commerce">Source Code</a>
           </div>
           <div className="bg-gray-800 p-6 rounded-xl shadow hover:shadow-lg">
-            <h4 className="text-xl font-semibold">Project 2</h4>
-            <p className="mt-2 text-gray-400">Another awesome project with React and MongoDB.</p>
+            <h4 className="text-xl font-semibold">Album Player</h4>
+            <p className="mt-2 text-gray-400">A Music Player Shows Music By Albums And Built In JavaScript.</p>
+            <a className="underline" href="https://github.com/rritikyadav/albumplayer">Source Code</a>
           </div>
           <div className="bg-gray-800 p-6 rounded-xl shadow hover:shadow-lg">
-            <h4 className="text-xl font-semibold">Project 3</h4>
-            <p className="mt-2 text-gray-400">An API-based app using Node.js and Express.</p>
+            <h4 className="text-xl font-semibold">React To-Do</h4>
+            <p className="mt-2 text-gray-400">A To-Do app with Proper Backend Built In React , MongoDb and Express.</p>
+            <a className="underline mr-10" href="https://github.com/rritikyadav/react-todo">Frontend</a>
+            <a className="underline" href="https://github.com/rritikyadav/react-todo-server">Backend</a>
           </div>
         </div>
       </div>
